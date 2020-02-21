16c6effee15e1023dc260986f1967bbc556bc920
saltanovst <ss@get-net.ru>
2020-02-21 17:31:09 +0500

    ADD basic autotesting. WIP

--- openapi.lua
+++ openapi.lua
@@ -1,4 +1,5 @@
 -- tnt imports
+local base64_encode = require("digest").base64_encode
 local base64_decode = require("digest").base64_decode
 local uuid          = require("uuid")
 local fio           = require("fio")
@@ -7,6 +8,11 @@ local fun           = require("fun")
 -- helpers
 local sprintf = string.format
 
+--[[
+    set variables for test cases beforehand
+    kinda lazy-loading
+]]
+local json, tap, log, uri
 
 local function read_spec(spec_path)
     assert(
@@ -33,6 +39,7 @@ end
 local _M = {}
 local _V = {}
 local _U = {}
+local _T = {}
 local mt = {}
 
 function mt:__call(httpd, spec_path, options)
@@ -47,6 +54,11 @@ function mt:__call(httpd, spec_path, options)
     httpd.default                = _U.httpd_default_handler
     httpd.error_handler          = _U.httpd_error_handler
     httpd.security_error_handler = _U.httpd_security_error_handler
+
+    _T.httpd_start = httpd.start
+    _T.httpd_stop  = httpd.stop
+
+    httpd.start = _T.start
 
     for k, v in next, options do
         rawset(httpd.options, k, v)
@@ -95,7 +107,7 @@ function _M:new(spec)
                             options = self.components.securitySchemes[key]
                             if not options then
                                 error(sprintf(
-                                    "Schema %s is not described in securityScheme",
+                                    "Schema %s is not described in securitySchemes",
                                     key
                                 ))
                             end
@@ -467,7 +479,7 @@ function _V.validate(ctx)
     end
 
     if ctype then
-        local with_charset = ctype:match("(%w+/%w+);[%w+]")
+        local with_charset = ctype:match("(%S+)[;]:?")
 
         if with_charset then
             ctype = with_charset
@@ -777,6 +789,261 @@ function _V.p_error(alias, expected, actual)
             actual = actual
         }
     }
+end
+
+function _T.start(ctx)
+    if fun.any(function(val) return val == "--test" end, arg) then
+        return _T.run(ctx)
+    end
+
+    return _T.httpd_start(ctx)
+end
+
+function _T.set_env(ctx)
+    log.info("RUNNING TESTS:\n")
+
+    _T.test = tap.test("openapi-schema")
+    local test_count = fun.reduce(
+        function(res)
+            res = res +1
+            return res
+        end,
+        0,
+        ctx.openapi.paths
+    )
+
+    _T.test:plan(test_count)
+
+    local server_settings = assert(ctx.openapi.servers)
+    server_settings = fun.filter(
+        function(val)
+            return val.description == "test"
+        end,
+        server_settings
+    ):totable()[1]
+
+    if not server_settings then
+        error("Test server settings not set")
+    end
+
+    _T.server_settings = uri.parse(server_settings.url)
+
+    return
+end
+
+function _T.run(ctx)
+    -- set variables set before to respective modules
+    json, tap, log, uri = require("json"), require("tap"), require("log"), require("net.url")
+
+    -- set local http-client instance just for testing purposes and nothing else
+    _T.client = require("http.client").new({5})
+
+    -- sets the testing env
+    _T.set_env(ctx)
+
+    -- overrides server settings from openapi schema
+    ctx.host = _T.server_settings.host
+    ctx.port = _T.server_settings.port
+
+    -- shutdown request loggin to not get unwanted io data
+    ctx.options.log_requests = false
+
+    -- starts the mock server
+    _T.httpd_start(ctx)
+
+    _T.run_path_tests(ctx)
+
+    log.info("\nTesting complete. Shutting down...\n")
+    _T.httpd_stop(ctx)
+    os.exit(0)
+end
+
+-- decoding by response's content-type, maybe there'll be more of those later(xml or whatever)
+function _T.json(resp)
+    local status, decoded = pcall(json.decode, resp.body)
+
+    if not status then
+        return nil, "Invalid JSON data: ".. decoded
+    end
+
+    return decoded
+end
+
+function _T.send_request(args)
+    return _T.client:request(unpack(args))
+end
+
+function _T.form_request(method, relpath, query, body, opts)
+    -- reuse already parsed and existing test server options
+    local full_path = table.deepcopy(_T.server_settings)
+    full_path.path = relpath
+    full_path.query = uri.buildQuery(query)
+
+    body = method ~= "GET" and json.encode(body) or ""
+
+
+    return {
+        method,
+        tostring(full_path),
+        body,
+        opts
+    }
+end
+
+function _T.form_security(ctx, headers, options)
+    -- there are also scopes in second value, but those are unneeded here
+    local option = next(options)
+
+    assert(ctx.openapi.components.securitySchemes, "securitySchemes component is not described")
+    local err_msg = ("Schema for %s security does not exist in securitySchemes component"):format(option)
+
+    local sec_schema = assert(ctx.openapi.components.securitySchemes[option], err_msg)
+
+    local sec_type = sec_schema.scheme or sec_schema.type
+
+    if sec_type == "bearer" then
+        local value = ("Bearer %s"):format(_T.test_config.security[option])
+        rawset(headers, "Authorization", value)
+    end
+
+    if sec_type == "apiKey" then
+        local value = _T.test_config.security[option]
+        rawset(headers, sec_schema.name, value)
+    end
+
+    if sec_type == "basic" then
+        assert(_T.test_config.security[option], "Basic authorization data is not set in tests/config.lua")
+        local username, password = _T.test_config.security[option].username, _T.test_config.security[option].password
+
+        local value = ("Basic %s"):format(
+            base64_encode(
+                ("%s:%s"):format(username, password)
+            )
+        )
+
+        rawset(headers, "Authorization", value)
+    end
+end
+
+function _T.form_expected(ctx, schema)
+    local is_properties = false
+    local result = {}
+    local ctype
+
+    ::redo::
+    for key, val in next, schema do
+        if key == "$ref" then
+            if not is_properties then
+                schema = ctx.openapi:ref(val)
+            else
+                key, val = next(ctx.openapi:ref(val))
+            end
+
+            goto redo
+        end
+
+        if schema.content then
+            ctype = next(schema.content)
+            schema = schema.content[ctype].schema.properties
+            is_properties = true
+            goto redo
+        end
+
+        if is_properties then
+            rawset(result, key, val.example)
+        end
+    end
+
+    return result
+end
+
+function _T.run_path_tests(ctx)
+    _T.test_config      = require("tests.config")
+
+
+    for path, options in next, ctx.openapi.paths do
+        for method, opts in next, options do
+            local headers = {}
+            local ctype
+
+            if opts.security then
+                for _, val in next, opts.security do
+                    _T.form_security(ctx, headers, val)
+                end
+            end
+
+            if opts.requestBody then
+                ctype = next(opts.requestBody.content)
+
+                local with_charset = ctype:match("(%S+)[;]:?")
+                if with_charset then
+                    ctype = with_charset
+                end
+
+                rawset(headers, "content-type", ctype)
+            end
+
+            local params = ctx.openapi:form_params(path, method, ctype)
+
+            local body, query = {}, {}
+            if method ~= "get" and params.body then
+                body = fun.map(
+                    function(name, vars)
+                        --[[
+                            check, whether this parameter is required
+                             then assert that it has an example value set in openapi schema
+                             throw an error otherwise
+                        ]]
+                        if fun.any(function(val) return val == name end,params.body.required) then
+                            local msg = ("Example variable not set for the %q parameter in %s %s"):format(name, method, path)
+                            assert(vars.example, msg)
+                        end
+                        return name, vars.example
+                    end,
+                    params.body.properties
+                ):tomap()
+            end
+
+            if params.query then
+                query = fun.map(
+                    function(vars)
+                        local schema_msg = ("Schema option not set for the %q parameter in %s %s"):format(vars.name, method, path)
+                        assert(vars.schema, schema_msg)
+                        if vars.required or vars['in'] == "path" then
+                            local msg = ("Example variable not set for the %q parameter in %s %s"):format(vars.name, method, path)
+                            assert(vars.schema.example, msg)
+                        end
+
+                        if vars['in'] == "query" then
+                            return vars.name, vars.schema.example
+                        else
+                            local pattern = ("{%s}"):format(vars.name)
+                            path = path:gsub(pattern, vars.schema.example)
+                        end
+
+                        return vars,name
+                    end,
+                    params.query
+                ):tomap()
+            end
+
+            -- curl takes only uppercase method, responses with protocol error otherwise
+            method = method:upper()
+
+            local request_data = _T.form_request(method, path, query, body, {headers = headers})
+
+            local resp = _T.send_request(request_data)
+
+            _T.test:ok(resp.status == 200, ("%s %s OK STATUS"):format(method, path))
+
+            local resp_body = _T.json(resp)
+            local resp_schema = opts.responses[resp.status]
+            _T.test:ok(resp_schema, ("%s RESPONSE SCHEMA EXISTS"):format(resp.status))
+
+            local expected = _T.form_expected(ctx, resp_schema)
+            _T.test:is_deeply(expected, resp_body, ("%s %s RESPONSE"):format(method, path))
+        end
+    end
 end
 
 return _M
