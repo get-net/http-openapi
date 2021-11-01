18a67cf28b7f7c19ad32d003b1afa9824e715483
saltanovst <ss@get-net.ru>
2021-11-01 13:52:26 +0500

    FIX a cookie workaround

--- src/util.lua
+++ src/util.lua
@@ -136,9 +136,7 @@ function _M.apiKey(ctx, name, goes_in)
         local n = name:lower()
         return ctx:header(n)
     elseif goes_in == "cookie" then
-        local val = ctx:header("cookie")
-
-        return val and val:match(("%s=([^;]*)"):format(name)) or nil
+		return ctx:cookie(name)
     end
 end
 
